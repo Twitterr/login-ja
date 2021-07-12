de2889338a166637c3f8c60d07c417fbc7206e12
Twitterr <87309826+Twitterr@users.noreply.github.com>
2021-07-12 19:50:53 +0900

    Add files via upload

---
+++ Twitter.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>Twitter</title>
+</head>
+<body>
+<table border="1">
+  <tr>
+    <td>ユーザー名</td><td><?php echo $_POST["name"] ?></td>
+  </tr>
+  <tr>
+    <td>パスワード</td><td><?php echo $_POST["mail"] ?></td>
+  </tr>
+  <form action="#" method="post">
+</table>
+</body>
+</html>
